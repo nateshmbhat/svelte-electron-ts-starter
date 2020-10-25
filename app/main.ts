5bcbd1e5f8955b6d1afda8e9d30c436b35a9d3e5
nateshmbhat <nateshmbhat1@gmail.com>
2020-10-25 15:46:14 +0530

    updated

--- app/main.ts
+++ app/main.ts
@@ -1,57 +1,60 @@
-import * as path from 'path';
-import * as url from 'url';
-import { app, BrowserWindow } from 'electron';
+import * as path from "path";
+import * as url from "url";
+import { app, BrowserWindow } from "electron";
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
+let win : BrowserWindow;
 
-const mode = process.env.NODE_ENV;
-require('electron-reload')(__dirname);
+const production: boolean =
+  (process.env.NODE_ENV || "production") === "production";
+if (!production) {
+  require("electron-reload")(__dirname);
+}
 
 function launch() {
-	win = new BrowserWindow({
-		width: 800,
-		height: 600,
-		minWidth: 600,
-		backgroundColor: 'white',
-		titleBarStyle: 'hidden',
-		webPreferences:{
-			nodeIntegration:true
-		}
-	});
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 600,
+    backgroundColor: "white",
+    titleBarStyle: "hidden",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-	win.loadURL(
-		url.format({
-			pathname: path.join(__dirname, '../index.html'),
-			protocol: 'file:',
-			slashes: true
-		})
-	);
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "../index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
 
-	win.on('closed', function() {
-		win = null;
-	});
+  win.on("closed", function () {
+    win = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', launch);
+app.on("ready", launch);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+app.on("window-all-closed", function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-app.on('activate', function() {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (win === null) {
-		launch();
-	}
+app.on("activate", function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    launch();
+  }
 });
